6ddfa7b62974ac0a2b71dc73284dd67279052425
Drew Strojny <dstrojny@jestro.com>
2012-01-20 11:04:15 -0700

    First commit.

---
+++ config.rb
@@ -0,0 +1,50 @@
+# The name of the theme
+config[:name] = "twentytwelve"
+
+# The website for the theme
+config[:uri] = ""
+
+# The author's name
+config[:author] = ""
+
+# The author's website
+config[:author_uri] = ""
+
+# Description of the theme
+config[:description] = ""
+
+# Version number of the theme
+config[:version_number] = ".5"
+
+# Name of the theme license
+config[:license_name] = "GPL"
+
+# Website for the theme license
+config[:license_uri] = ""
+
+# Tags for this theme, as an array (ex. ["blue", "white", "two-columns"])
+config[:tags] = []
+
+# Additional comments (optional)
+config[:comments] = ""
+
+# JavaScript compression
+# config[:compress_js] = false
+
+# Enable livereload
+config[:livereload] = true
+
+# Set a higher precision
+Sass::Script::Number.precision(9)
+
+# Compass configuration can also go here.
+# See http://compass-style.org/help/tutorials/configuration-reference/ for some of the options
+# Note: Most options (especially path-related options) will have no effect on Forge
+
+# Compass.configuration do |compass|
+#   compass.line_comments = true
+#   compass.output_style = :expanded # use :compressed for minified version
+# end
+
+# You can also include additional Compass frameworks by requiring them:
+# require 'stitch'
