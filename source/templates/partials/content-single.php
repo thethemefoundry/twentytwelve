c50b9af5156a2c2f1a621430ed556ed1b9160675
Drew Strojny <dstrojny@jestro.com>
2012-02-13 16:16:34 -0700

    Tie in the single view. References #7.

--- source/templates/partials/content-single.php
+++ source/templates/partials/content-single.php
@@ -11,12 +11,6 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<h1 class="entry-title"><?php the_title(); ?></h1>
-
-			<?php if ( 'post' == get_post_type() ) : // Hide entry meta for pages ?>
-			<div class="entry-meta">
-				<?php twentytwelve_posted_on(); ?>
-			</div><!-- .entry-meta -->
-			<?php endif; ?>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
