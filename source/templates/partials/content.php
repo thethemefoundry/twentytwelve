69f8af6f11c62bed353c130721842b9d5826ce55
Drew Strojny <dstrojny@jestro.com>
2012-02-13 16:02:53 -0700

    Refine post headers and footers. Closes #7.

--- source/templates/partials/content.php
+++ source/templates/partials/content.php
@@ -14,9 +14,11 @@
 
 			<?php if ( 'post' == get_post_type() ) : // Hide entry meta for pages ?>
 			<div class="entry-meta">
-				<?php twentytwelve_posted_on(); ?>
+				by Drew Strojny
 			</div><!-- .entry-meta -->
 			<?php endif; ?>
+			<?php if ( comments_open() ) : ?>
+			<?php endif; // End if comments_open() ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( is_search() ) : // Only display excerpts for search ?>
@@ -27,6 +29,7 @@
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+			<!--<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>-->
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
@@ -37,24 +40,21 @@
 				$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
 				if ( $categories_list ) :
 			?>
-			<span class="cat-links">
-				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentytwelve' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list ); ?>
-			</span>
+			<div class="category-and-date">
+				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentytwelve' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list ); ?> on <?php the_date(); ?>
+			</div>
 			<?php endif; // End if categories ?>
 			<?php
 				/* translators: used between list items, there is a space after the comma */
 				$tags_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
 				if ( $tags_list ) : ?>
-			<span class="tag-links">
+			<div class="tag-links">
 				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentytwelve' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-			</span>
+			</div>
 			<?php endif; // End if $tags_list ?>
 			<?php endif; // End if 'post' == get_post_type() ?>
-
-			<?php if ( comments_open() ) : ?>
-			<span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '<b>1</b> Reply', 'twentytwelve' ), __( '<b>%</b> Replies', 'twentytwelve' ) ); ?></span>
-			<?php endif; // End if comments_open() ?>
-
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+			<div class="comments-link">
+			<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+			</div>
 		</footer><!-- #entry-meta -->
 	</article><!-- #post -->
