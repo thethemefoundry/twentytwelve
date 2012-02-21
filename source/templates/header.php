57db15e8b3fc6a9c7dbdc1d49aef379dedadfc63
Lance Willett <lance@simpledream.net>
2012-02-20 22:27:11 -0700

    Header: sync with core version
    Add site title to title area and add in extra class values

--- source/templates/header.php
+++ source/templates/header.php
@@ -13,7 +13,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
@@ -57,8 +57,8 @@ wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri() );
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed">
-	<header id="masthead" role="banner">
+<div id="page" class="site hfeed">
+	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
