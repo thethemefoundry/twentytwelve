0f7fef2a49581f7f572879107a3dc85262fdb5c6
Lance Willett <lance@simpledream.net>
2012-02-20 22:10:41 -0700

    Content templates: update header/footer to use new helper functions
    This way we don't repeat complex code across multiple files, just do it once in functions.php
    
    References #7

--- source/templates/partials/content-single.php
+++ source/templates/partials/content-single.php
@@ -11,6 +11,9 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<div class="entry-meta">
+				<?php twentytwelve_posted_by() ?>
+			</div><!-- .entry-meta -->
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
@@ -19,31 +22,15 @@
 		</div><!-- .entry-content -->
 
 		<footer class="entry-meta">
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
+			<?php twentytwelve_entry_meta(); ?>
 
-				/* translators: used between list items, there is a space after the comma */
-				$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
-				if ( '' != $tag_list ) {
-					$utility_text = __( 'This entry was posted in %1$s and tagged %2$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentytwelve' );
-				} elseif ( '' != $categories_list ) {
-					$utility_text = __( 'This entry was posted in %1$s by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentytwelve' );
-				} else {
-					$utility_text = __( 'This entry was posted by <a href="%6$s">%5$s</a>. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'twentytwelve' );
-				}
+			<?php if ( comments_open() ) : ?>
+			<span class="comments-link">
+				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+			</span>
+			<?php endif; ?>
 
-				printf(
-					$utility_text,
-					$categories_list,
-					$tag_list,
-					esc_url( get_permalink() ),
-					the_title_attribute( 'echo=0' ),
-					get_the_author(),
-					esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
-				);
-			?>
-			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
+			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link"><span class="sep"> | </span>', '</span>' ); ?>
 
 			<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
 			<div id="author-info">
--- source/templates/partials/content.php
+++ source/templates/partials/content.php
@@ -11,20 +11,9 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
-			<?php if ( 'post' == get_post_type() ) : // Hide entry meta for pages ?>
 			<div class="entry-meta">
-			<?php
-				printf( __( '<span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span></span>', 'twentytwelve' ),
-					esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-					esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve' ), get_the_author() ) ),
-					get_the_author()
-				);
-			?>
+				<?php twentytwelve_posted_by() ?>
 			</div><!-- .entry-meta -->
-			<?php endif; ?>
-			<?php if ( comments_open() ) : ?>
-			<?php endif; // End if comments_open() ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( is_search() ) : // Only display excerpts for search ?>
@@ -35,32 +24,18 @@
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-			<!--<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>-->
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
 		<footer class="entry-meta">
-			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
-				if ( $categories_list ) :
-			?>
-			<div class="category-and-date">
-				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentytwelve' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list ); ?> on <?php the_date(); ?>
-			</div>
-			<?php endif; // End if categories ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
-				if ( $tags_list ) : ?>
-			<div class="tag-links">
-				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentytwelve' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
-			</div>
-			<?php endif; // End if $tags_list ?>
-			<?php endif; // End if 'post' == get_post_type() ?>
-			<div class="comments-link">
-			<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-			</div>
+			<?php twentytwelve_entry_meta(); ?>
+
+			<?php if ( comments_open() ) : ?>
+			<span class="comments-link">
+				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+			</span>
+			<?php endif; ?>
+
+			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link"><span class="sep"> | </span>', '</span>' ); ?>
 		</footer><!-- #entry-meta -->
 	</article><!-- #post -->
