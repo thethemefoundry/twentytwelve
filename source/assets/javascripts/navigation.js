c603ed2d28b4e48628b3cd7bf09aacb5a9e7e0c9
Lance Willett <lance@simpledream.net>
2012-03-28 11:43:22 -0700

    Navigation.js - restore to latest core version
    See http://core.trac.wordpress.org/browser/trunk/wp-content/themes/twentytwelve/js/navigation.js?rev=20007
    
    The core version was missed during the merge back to Github (my fault)

--- source/assets/javascripts/navigation.js
+++ source/assets/javascripts/navigation.js
@@ -4,8 +4,8 @@
  * Handles toggling the navigation menu for small screens.
  */
 jQuery( document ).ready( function( $ ) {
-	var browserWidth = $( window ).width(),
-	    masthead = $( '#masthead' );
+	var masthead = $( '#masthead' ),
+	    timeout = false;
 
 	$.fn.smallMenu = function() {
 		masthead.find( '.site-navigation' ).removeClass( 'main-navigation' ).addClass( 'main-small-navigation' );
@@ -17,17 +17,25 @@ jQuery( document ).ready( function( $ ) {
 		} );
 	};
 
-	$( window ).resize( function() {
-		if ( browserWidth < 600 ) {
-			$.fn.smallMenu();
-		} else {
-			masthead.find( '.site-navigation' ).removeClass( 'main-small-navigation' ).addClass( 'main-navigation' );
-			masthead.find( '.site-navigation h3' ).removeClass( 'menu-toggle' ).addClass( 'assistive-text' );
-			masthead.find( '.menu' ).removeAttr( 'style' );
-		}
-	} );
-
-	if ( browserWidth < 600 ) {
+	// Check viewport width on first load.
+	if ( $( window ).width() < 600 )
 		$.fn.smallMenu();
-	}
+
+	// Check viewport width when user resizes the browser window.
+	$( window ).resize( function() {
+		var browserWidth = $( window ).width();
+
+		if ( false !== timeout )
+			clearTimeout( timeout );
+
+		timeout = setTimeout( function() {
+			if ( browserWidth < 600 ) {
+				$.fn.smallMenu();
+			} else {
+				masthead.find( '.site-navigation' ).removeClass( 'main-small-navigation' ).addClass( 'main-navigation' );
+				masthead.find( '.site-navigation h3' ).removeClass( 'menu-toggle' ).addClass( 'assistive-text' );
+				masthead.find( '.menu' ).removeAttr( 'style' );
+			}
+		}, 200 );
+	} );
 } );
