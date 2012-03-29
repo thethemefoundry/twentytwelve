7db021d0b454a3b03fada3d6eea9eb226c1d4995
obenland <obenland@gmx.de>
2012-03-29 14:04:39 +0300

    Always show a message when comments are closed (except on pages), to adhere to Theme Unit Test Guidelines: http://codex.wordpress.org/Theme_Unit_Test#Comments_Disabled

--- source/templates/single.php
+++ source/templates/single.php
@@ -15,11 +15,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template( '', true );
-				?>
+				<?php comments_template( '', true ); ?>
 
 			<?php endwhile; // end of the loop. ?>
 
