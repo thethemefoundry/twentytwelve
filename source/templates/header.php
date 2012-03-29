b9b064e2d4fc0101777e53434d094a0b2ae06e80
obenland <obenland@gmx.de>
2012-03-29 14:15:53 +0300

    Move scripts and styles to functions.php and enqueue them there properly

--- source/templates/header.php
+++ source/templates/header.php
@@ -44,14 +44,7 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/javascripts/html5.js" type="text/javascript"></script>
 <![endif]-->
-<?php
-if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
-	wp_enqueue_script( 'comment-reply' );
 
-wp_enqueue_style( 'fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' );
-
-wp_enqueue_style( 'twentytwelve-style', get_stylesheet_uri() );
-?>
 <?php wp_head(); ?>
 </head>
 
