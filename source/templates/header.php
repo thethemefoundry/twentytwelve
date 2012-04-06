81874c700d34b14e021b0fe98ec0ebe824c0c92e
Jeff Sebring <jeff@jeffsebring.com>
2012-04-05 23:15:47 -0700

    ask browsers for latest rendering engine + prevent mobile browser scaling

--- source/templates/header.php
+++ source/templates/header.php
@@ -15,7 +15,8 @@
 <!--[if gt IE 8]> <!--> <html class="no-js" <?php language_attributes( 'html' ); ?>>                 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
