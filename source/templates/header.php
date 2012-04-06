3c07878faa1e7b3eb715fcc3978aa4a26ffb40ff
Jeff Sebring <jeff@jeffsebring.com>
2012-04-05 22:30:39 -0700

    conditional html classes

--- source/templates/header.php
+++ source/templates/header.php
@@ -8,8 +8,11 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+?><!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes( 'html' ); ?>>  <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes( 'html' ); ?>>         <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes( 'html' ); ?>>                <![endif]-->
+<!--[if gt IE 8]> <!--> <html class="no-js" <?php language_attributes( 'html' ); ?>>                 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
