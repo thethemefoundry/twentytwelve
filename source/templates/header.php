c689da283dfb22d759d4efce0e752ead37bf0213
Lance Willett <lance@simpledream.net>
2012-04-18 08:57:44 -0700

    Add initial-scale to viewport definition
    Props jeffsebring for the nudge, see https://github.com/thethemefoundry/twentytwelve/pull/20

--- source/templates/header.php
+++ source/templates/header.php
@@ -12,7 +12,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php wp_title( '|', true, 'right' ); ?> <?php bloginfo( 'name' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
