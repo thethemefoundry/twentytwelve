7598373975375030bdc41b4c65826572aafaa949
Lance Willett <lance@simpledream.net>
2012-05-16 12:09:43 -0700

    Sync latest changes to build dir

--- build/homepage.php
+++ build/homepage.php
@@ -8,21 +8,23 @@ get_header(); ?>
 	<div id="primary">
 		<div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php if ( has_post_thumbnail() ) { ?>
-					<div class="entry-page-image">
-						<?php the_post_thumbnail(); ?>
-					</div>
-				<?php } ?>
-				<section class="home-top<?php echo ( has_post_thumbnail() ) ? ' thumbnail' : ''; ?>">
-					<div class="entry-content">
-						<header class="entry-header">
-							<h1 class="entry-title"><?php the_title(); ?></h1>
-						</header><!-- .entry-header -->
-						<?php the_content(); ?>
-					</div><!-- .entry-content -->
-				</section>
-			<?php endwhile; // end of the loop. ?>
+			<div class="home-top">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php if ( has_post_thumbnail() ) { ?>
+						<div class="entry-page-image">
+							<?php the_post_thumbnail(); ?>
+						</div>
+					<?php } ?>
+					<section class="home-content<?php echo ( has_post_thumbnail() ) ? ' thumbnail' : ''; ?>">
+						<div class="entry-content">
+							<header class="entry-header">
+								<h1 class="entry-title"><?php the_title(); ?></h1>
+							</header><!-- .entry-header -->
+							<?php the_content(); ?>
+						</div><!-- .entry-content -->
+					</section>
+				<?php endwhile; // end of the loop. ?>
+			</div>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
