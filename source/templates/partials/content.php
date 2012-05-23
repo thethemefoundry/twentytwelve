0dc18574ad3e191e17b837a672e34acf2a20a1e0
Drew Strojny <dstrojny@jestro.com>
2012-05-23 10:08:42 -0600

    Move comment count to post header.

--- source/templates/partials/content.php
+++ source/templates/partials/content.php
@@ -16,9 +16,11 @@
 			<?php else : ?>
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 			<?php endif; // is_single() ?>
-			<div class="entry-meta">
-				<?php twentytwelve_posted_by() ?>
-			</div><!-- .entry-meta -->
+			<?php if ( comments_open() ) : ?>
+				<div class="comments-link">
+					<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
+				</div><!-- .comments-link -->
+			<?php endif; // comments_open() ?>
 		</header><!-- .entry-header -->
 
 		<div class="entry-content">
@@ -29,31 +31,21 @@
 		<footer class="entry-meta">
 			<?php twentytwelve_entry_meta(); ?>
 			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-
-			<?php if ( ! is_single() ) : // Displaying index, archive, search ?>
-			<?php if ( comments_open() ) : ?>
-			<span class="sep"> | </span>
-			<span class="comments-link">
-				<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentytwelve' ) . '</span>', __( '1 Reply', 'twentytwelve' ), __( '% Replies', 'twentytwelve' ) ); ?>
-			</span>
-			<?php endif; // comments_open() ?>
-			<?php else : // Displaying single posts ?>
 			<?php if ( get_the_author_meta( 'description' ) && ( ! function_exists( 'is_multi_author' ) || is_multi_author() ) ) : // If a user has filled out their description and this is a multi-author blog, show a bio on their entries ?>
-			<div id="author-info">
-				<div id="author-avatar">
-					<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentytwelve_author_bio_avatar_size', 68 ) ); ?>
-				</div><!-- #author-avatar -->
-				<div id="author-description">
-					<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
-					<?php the_author_meta( 'description' ); ?>
-					<div id="author-link">
-						<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-							<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
-						</a>
-					</div><!-- #author-link	-->
-				</div><!-- #author-description -->
-			</div><!-- #author-info -->
-				<?php endif; ?>
-			<?php endif; // is_single() ?>
+				<div id="author-info">
+					<div id="author-avatar">
+						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentytwelve_author_bio_avatar_size', 68 ) ); ?>
+					</div><!-- #author-avatar -->
+					<div id="author-description">
+						<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
+						<?php the_author_meta( 'description' ); ?>
+						<div id="author-link">
+							<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
+								<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
+							</a>
+						</div><!-- #author-link	-->
+					</div><!-- #author-description -->
+				</div><!-- #author-info -->
+			<?php endif; ?>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
