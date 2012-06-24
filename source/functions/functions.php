be43696f07f78077758c164bcefca42258f58b97
Lance Willett <lance@simpledream.net>
2012-06-24 13:02:19 -0700

    Separate custom header code into a new file and add back-compat for pre 3.4 versions of WP.
    Props mfields for work with _s which inspired this change.

--- source/functions/functions.php
+++ source/functions/functions.php
@@ -69,136 +69,16 @@ function twentytwelve_setup() {
 	// Add support for custom background.
 	add_theme_support( 'custom-background' );
 
-	// Add support for a custom header image.
-	add_theme_support( 'custom-header', array(
-		// The default header text color.
-		'default-text-color' => '444',
-		// Random image rotation by default.
-		'random-default' => true,
-		// Support flexible height and width.
-		'flex-height' => true,
-		'flex-width' => true,
-		// Set height and width, with a maximum value for the width.
-		'height' => apply_filters( 'twentytwelve_header_image_height', 250 ),
-		'width' => apply_filters( 'twentytwelve_header_image_width', 960 ),
-		'max-width' => apply_filters( 'twentytwelve_header_image_max_width', 2000 ),
-		// Callback for styling the header.
-		'wp-head-callback' => 'twentytwelve_header_style',
-		// Callback for styling the header preview in the admin.
-		'admin-head-callback' => 'twentytwelve_admin_header_style',
-		// Callback used to display the header preview in the admin.
-		'admin-preview-callback' => 'twentytwelve_admin_header_image',
-	) );
-
 	// Add custom image size for featured image use, displayed on "standard" posts.
 	add_theme_support( 'post-thumbnails' );
 	set_post_thumbnail_size( 624, 9999 ); // Unlimited height, soft crop
 }
 add_action( 'after_setup_theme', 'twentytwelve_setup' );
 
-if ( ! function_exists( 'twentytwelve_header_style' ) ) :
 /**
- * Styles the header image and text displayed on the blog
- *
- * get_header_textcolor() options: 444 is default, hide text (returns 'blank'), or any hex value
- *
- * @since Twenty Twelve 1.0
+ * Add support for a custom header image.
  */
-function twentytwelve_header_style() {
-	$text_color = get_header_textcolor();
-	// If no custom options for text are set, let's bail
-	if ( $text_color == get_theme_support( 'custom-header', 'default-text-color' ) )
-		return;
-	// If we get this far, we have custom styles.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute !important;
-			clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text, use that.
-		else :
-	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo $text_color; ?> !important;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
-}
-endif;
-
-if ( ! function_exists( 'twentytwelve_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * Referenced via add_custom_image_header() in twentytwelve_setup().
- *
- * @since Twenty Twelve 1.0
- */
-function twentytwelve_admin_header_style() {
-?>
-	<style type="text/css">
-	.appearance_page_custom-header #headimg {
-		border: none;
-	}
-	#headimg h1,
-	#headimg h2 {
-		line-height: 1.6;
-		margin: 0;
-		padding: 0;
-	}
-	#headimg h1 {
-		font-size: 30px;
-	}
-	#headimg h1 a {
-		text-decoration: none;
-	}
-	#headimg h2 {
-		font: normal 13px/1.8 "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", sans-serif;
-		margin-bottom: 24px;
-	}
-	#headimg img {
-	}
-	</style>
-<?php
-}
-endif;
-
-if ( ! function_exists( 'twentytwelve_admin_header_image' ) ) :
-/**
- * Custom header image markup displayed on the Appearance > Header admin panel.
- *
- * Referenced via add_custom_image_header() in twentytwelve_setup().
- *
- * @since Twenty Twelve 1.0
- */
-function twentytwelve_admin_header_image() {
-	?>
-	<div id="headimg">
-		<?php
-		if ( ! display_header_text() )
-			$style = ' style="display:none;"';
-		else
-			$style = ' style="color:#' . get_header_textcolor() . ';"';
-		?>
-		<h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<h2 id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></h2>
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<img src="<?php echo esc_url( $header_image ); ?>" alt="" />
-		<?php endif; ?>
-	</div>
-<?php }
-endif;
+require( get_template_directory() . '/includes/custom-header.php' );
 
 /**
  * Enqueue scripts and styles for front-end.
