1965b522398ae0cd41ab19a7fa8a84bf68c102e7
Lance Willett <lance@simpledream.net>
2012-06-24 13:10:00 -0700

    Better indentation

--- source/templates/sidebar.php
+++ source/templates/sidebar.php
@@ -11,7 +11,7 @@
 ?>
 
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</div><!-- #secondary .widget-area -->
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</div><!-- #secondary .widget-area -->
 	<?php endif; ?>
