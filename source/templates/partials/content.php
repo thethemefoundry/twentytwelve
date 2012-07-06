3d7492f2e29874db515d827a5c5492754524fb93
Drew Strojny <dstrojny@jestro.com>
2012-07-05 18:26:55 -0600

    Add sticky post support. Closes #30.

--- source/templates/partials/content.php
+++ source/templates/partials/content.php
@@ -9,12 +9,19 @@
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php if (is_sticky() ) : ?>
+		<div class="featured-post">
+			<?php _e( 'Featured post', 'twentytwelve' ); ?>
+		</div>
+		<?php endif; // is_sticky() ?>
 		<header class="entry-header">
 			<?php the_post_thumbnail(); ?>
 			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h1 class="entry-title">
+				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+			</h1>
 			<?php endif; // is_single() ?>
 			<?php if ( comments_open() ) : ?>
 				<div class="comments-link">
